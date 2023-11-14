cae5daf686af746eccb59865732b9085f3932c0e
Yorke Rhodes <yorke@hyperlane.xyz>
2023-11-14 18:48:56 -0500

    Fix recipient left padding

--- src/components/MessageQuickstart.tsx
+++ src/components/MessageQuickstart.tsx
@@ -8,6 +8,13 @@ import CodeBlock from "@theme/CodeBlock";
 import Tabs from "@theme/Tabs";
 import TabItem from "@theme/TabItem";
 
+function hexLeftPad(s: string, bytes = 32) {
+  if (s.startsWith("0x")) {
+    s = s.slice(2);
+  }
+  return "0x" + s.padStart(bytes * 2, "0");
+}
+
 export default function MessageQuickstart() {
   const [originChain, setOriginChain] = useState("$ORIGIN");
   const [destinationChain, setDestinationChain] = useState("$DESTINATION");
@@ -17,8 +24,7 @@ export default function MessageQuickstart() {
   const rpc = testnetChainMetadata[originChain]?.rpcUrls[0].http ?? "$RPC_URL";
   const destinationDomain =
     testnetChainMetadata[destinationChain]?.domainId ?? "$DESTINATION_DOMAIN";
-  const recipient =
-    TestRecipientAddresses[destinationChain]?.TestRecipient ?? "$RECIPIENT";
+  const recipient = hexLeftPad(TestRecipientAddresses[destinationChain]?.TestRecipient ?? "$RECIPIENT");
 
   const explorer =
     testnetChainMetadata[originChain]?.blockExplorers[0].url ??
@@ -53,9 +59,15 @@ export default function MessageQuickstart() {
       <Tabs>
         <TabItem value="cast" label="Cast" default>
           <CodeBlock language="shell">
-            $ cast send {mailbox} "dispatch(uint32,bytes32,bytes)"
-            {destinationDomain} {recipient} $(cast --from-utf8 "{body}")
-            --rpc-url {rpc}
+            cast send {mailbox} "dispatch(uint32,bytes32,bytes)"{" \\\n"}
+            {"\t"}
+            {destinationDomain}
+            {" \\\n"}
+            {"\t"}
+            {recipient}
+            {" \\\n"}
+            {"\t"}$(cast --from-utf8 "{body}"){" \\\n"}
+            {"\t"}--rpc-url {rpc}
           </CodeBlock>
         </TabItem>
         <TabItem value="explorer" label="Explorer">
@@ -68,11 +80,11 @@ export default function MessageQuickstart() {
             </li>
             <li>
               Click on the <code>Connect to Web3</code> button to connect your
-              Wallet. Make sure you are on {originChain}.
+              Wallet. Make sure you are on <code>{originChain}</code>.
             </li>
             <li>
-              Fill in destination: <code>{destinationDomain}</code>, recipient: <code>
-              {recipient}</code>, and message: <code>{body}</code>
+              Fill in destination: <code>{destinationDomain}</code>, recipient:{" "}
+              <code>{recipient}</code>, and message: <code>{body}</code>
             </li>
             <li>
               Click the <code>Write</code> button to submit the transaction!
