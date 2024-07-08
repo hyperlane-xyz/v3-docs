a9b06591b16024e72dae20e9dd3ab01d09d0a7b4
-f <kunalarora1729@gmail.com>
2024-07-08 20:53:28 +0530

    fix redirect

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -184,47 +184,47 @@ const config = {
             ],
           },
           {
-            to: '/docs/protocol/mailbox',
-            from: ['/docs/protocol/messaging'],
+            to: "/docs/protocol/mailbox",
+            from: ["/docs/protocol/messaging"],
           },
           {
-            to: '/docs/protocol/ISM/modular-security',
-            from: ['/docs/protocol/sovereign-consensus'],
+            to: "/docs/protocol/ISM/modular-security",
+            from: ["/docs/protocol/sovereign-consensus"],
           },
           {
-            to: '/docs/protocol/economic-security',
-            from: ['/docs/protocol/proof-of-stake'],
+            to: "/docs/protocol/economic-security",
+            from: ["/docs/protocol/proof-of-stake"],
           },
           {
-            to: '/docs/protocol/agents/watchtowers',
-            from: ['/docs/protocol/agents/processor'],
+            to: "/docs/protocol/agents/watchtowers",
+            from: ["/docs/protocol/agents/processor"],
           },
           {
-            to: '/docs/protocol/ISM/ccip-read-ISM',
-            from: ['/docs/protocol/sovereign-consensus/ccip-read-ism'],
+            to: "/docs/protocol/ISM/ccip-read-ISM",
+            from: ["/docs/protocol/sovereign-consensus/ccip-read-ism"],
           },
           {
-            to: '/docs/protocol/ISM/multisig-ISM',
-            from: ['/docs/protocol/sovereign-consensus/multisig-ism'],
+            to: "/docs/protocol/ISM/multisig-ISM",
+            from: ["/docs/protocol/sovereign-consensus/multisig-ism"],
           },
           {
-            to: '/docs/protocol/ISM/aggregation-ISM',
-            from: ['/docs/protocol/sovereign-consensus/aggregation-ism'],
+            to: "/docs/protocol/ISM/aggregation-ISM",
+            from: ["/docs/protocol/sovereign-consensus/aggregation-ism"],
           },
           {
-            to: '/docs/protocol/ISM/routing-ISM',
-            from: ['/docs/protocol/sovereign-consensus/routing-ism'],
+            to: "/docs/protocol/ISM/routing-ISM",
+            from: ["/docs/protocol/sovereign-consensus/routing-ism"],
           },
           {
-            to: '/docs/protocol/ISM/third-party-ISMs/optimistic-ISM',
-            from: ['/docs/protocol/sovereign-consensus/optimistic-ism'],
+            to: "/docs/protocol/ISM/third-party-ISMs/optimistic-ISM",
+            from: ["/docs/protocol/sovereign-consensus/optimistic-ism"],
           },
           {
-            to: '/docs/protocol/ISM/third-party-ISMs/wormhole-ISM',
-            from: ['/docs/protocol/sovereign-consensus/wormhole-ism'],
+            to: "/docs/protocol/ISM/third-party-ISMs/wormhole-ISM",
+            from: ["/docs/protocol/sovereign-consensus/wormhole-ism"],
           },
           {
-            to: '/docs/faq',
+            to: "/docs/faq",
             from: [
               "/docs/operators/agent-keys",
               "/docs/operators/agent-keys/hexadecimal-key-setup",
@@ -270,7 +270,7 @@ const config = {
             from: ["/docs/protocol/sovereign-consensus"],
           },
           {
-            to: "/docs/protocol/ISM/economic-security",
+            to: "/docs/protocol/economic-security",
             from: ["/docs/protocol/proof-of-stake"],
           },
           {
