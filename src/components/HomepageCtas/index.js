ec737d2f3fed3669eaa7fd62ec66318938435dd6
Connor McEwen <connor.mcewen@gmail.com>
2024-07-15 19:49:40 -0400

    feat: update homepage to CTAs

---
+++ src/components/HomepageCtas/index.js
@@ -0,0 +1,65 @@
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
+import Link from "@docusaurus/Link";
+import links from "@site/src/links/links";
+
+const CtaList = [
+  {
+    title: "Deploy to a New Chain",
+    link: links.deployHyperlane,
+  },
+  {
+    title: "Bridge Tokens with Warp Routes",
+    link: links.deployWarpRoute,
+  },
+  {
+    title: "Learn Concepts",
+    link: links.protocolOverview,
+  },
+  {
+    title: "Run a Validator",
+    link: links.runValidators,
+  },
+  {
+    title: "Where is Hyperlane?",
+    link: links.contractAddresses,
+  },
+];
+
+function Cta({ title, link }) {
+  return (
+    <div class="col col--4 padding-vert--md">
+      <Link to={link}>
+        <button className="button button--lg button--secondary button--block">
+          {title}
+        </button>
+      </Link>
+    </div>
+  );
+}
+
+export default function HomepageCtas() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        {splitArray(CtaList, 3).map((row, idx) => (
+          <div className="row">
+            {row.map((props, idx) => (
+              <Cta key={idx} {...props} />
+            ))}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+function splitArray(array, n) {
+  let [...arr] = array;
+  var res = [];
+  while (arr.length) {
+    res.push(arr.splice(0, n));
+  }
+  return res;
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,34 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageCtas from "@site/src/components/HomepageCtas";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Discover Hyperlane
-          </Link>
-        </div>
+        <div className={styles.buttons}></div>
       </div>
     </header>
   );
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Welcome to ${siteConfig.title}`}
-      description="Permissionless Interoperability for modular rollups | Celestia, OP Stack, Arbitrum Orbit">
+      description="Permissionless Interoperability for modular rollups | Celestia, OP Stack, Arbitrum Orbit"
+    >
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <HomepageCtas />
       </main>
     </Layout>
   );
