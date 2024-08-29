835803a51f99b787a9abf1cfd04a7d0ad37b4bac
J M Rossy <jm.rossy@gmail.com>
2024-08-29 18:45:16 -0400

    Filter AW chains to those with mailboxes

--- src/utils/registry.ts
+++ src/utils/registry.ts
@@ -1,16 +1,26 @@
-import { chainMetadata } from "@hyperlane-xyz/registry";
+import { chainAddresses, chainMetadata } from "@hyperlane-xyz/registry";
 import type { ChainMetadata, ChainName } from "@hyperlane-xyz/sdk";
 import { useMemo } from "react";
 
 const ABACUS_WORKS_DEPLOYER_NAME = "abacus works";
 
 // Chains that Abacus Works has deployed (formerly known as 'core' chains)
-export function getAbacusWorksChains(isTestnet = false): ChainMetadata[] {
-  return Object.values(chainMetadata).filter(
-    (metadata: ChainMetadata) =>
+export function getAbacusWorksChains(
+  isTestnet = false,
+  requireMailbox = true
+): ChainMetadata[] {
+  return Object.values(chainMetadata).filter((metadata: ChainMetadata) => {
+    // Filter to chains whose deployer is Abacus Works
+    const isRightDeployer =
       metadata.deployer?.name.trim().toLowerCase() ===
-        ABACUS_WORKS_DEPLOYER_NAME && !!metadata.isTestnet === isTestnet
-  );
+      ABACUS_WORKS_DEPLOYER_NAME;
+    // Filter to only mainnets or testnets
+    const isRightChainType = !!metadata.isTestnet === isTestnet;
+    // If required, filter to chains that have a mailbox addresses in the registry
+    const hasMailboxAddress =
+      !requireMailbox || !!chainAddresses[metadata.name]?.mailbox;
+    return isRightDeployer && isRightChainType && hasMailboxAddress;
+  });
 }
 
 export function getAbacusWorksChainNames(isTestnet = false): ChainName[] {
