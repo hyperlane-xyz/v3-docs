bbb0a5ef60707b0f90805171902c69720983df9e
Paul Balaji <10051819+paulbalaji@users.noreply.github.com>
2024-10-16 11:23:38 +0100

    feat: update reorg periods (#220)
    * feat: update reorg periods in docs
    
    * move to reference + update redirects
    
    * tidy up ReorgPeriodTable component
    
    Signed-off-by: pbio <10051819+paulbalaji@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: pbio <10051819+paulbalaji@users.noreply.github.com>

---
+++ src/components/ReorgPeriodTable.tsx
@@ -0,0 +1,37 @@
+import { chainMetadata } from "@hyperlane-xyz/registry";
+import { useAbacusWorksChainNames } from "../utils/registry";
+import { capitalize, Environment } from "./AddressTable";
+
+type Props = {
+  environment: Environment;
+};
+
+export default function ReorgPeriodTable({ environment }: Props) {
+  const chainNames = useAbacusWorksChainNames(environment !== "mainnet");
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Chain</th>
+          <th>Domain</th>
+          <th>Reorg Period (blocks)</th>
+          <th>Estimated Block Time (seconds)</th>
+        </tr>
+      </thead>
+      <tbody>
+        {chainNames.map((chain) => {
+          const targetMetadata = chainMetadata[chain];
+          return (
+            <tr key={chain}>
+              <td>{targetMetadata.displayName ?? capitalize(chain)}</td>
+              <td>{targetMetadata.domainId}</td>
+              <td>{targetMetadata.blocks.reorgPeriod}</td>
+              <td>{targetMetadata.blocks.estimateBlockTime}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
