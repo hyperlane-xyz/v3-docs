7c87c32eac9ff2dcce838167b725e4b8f45a2dc7
Eda Akturk <edaakturk@192.168.1.112>
2024-11-01 13:44:56 +0300

    Update landing page

--- src/components/HomepageCtas/index.js
+++ src/components/HomepageCtas/index.js
@@ -1,65 +1,59 @@
 import React from "react";
-import clsx from "clsx";
 import styles from "./styles.module.css";
 import Link from "@docusaurus/Link";
 import links from "@site/src/links/links";
 
-const CtaList = [
-  {
-    title: "Deploy to a New Chain",
-    link: links.deployHyperlane,
-  },
-  {
-    title: "Bridge Tokens with Warp Routes",
-    link: links.deployWarpRoute,
-  },
-  {
-    title: "Run a Validator",
-    link: links.runValidators,
-  },
-  {
-    title: "Learn Concepts",
-    link: links.protocolOverview,
-  },
-  {
-    title: "Where is Hyperlane?",
-    link: links.contractAddresses,
-  },
-];
-
-function Cta({ title, link }) {
+function Home() {
   return (
-    <div class="col col--4 padding-vert--md">
-      <Link to={link}>
-        <button className="button button--lg button--secondary button--block">
-          {title}
-        </button>
-      </Link>
-    </div>
-  );
-}
-
-export default function HomepageCtas() {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        {splitArray(CtaList, 3).map((row, idx) => (
-          <div className="row">
-            {row.map((props, idx) => (
-              <Cta key={idx} {...props} />
-            ))}
+    <main className={styles.main}>
+      <br />
+      <section className={styles.features}>
+        <div className="container">
+          <h1
+            className="landing-tagline"
+            style={{
+              fontSize: "xx-large",
+              paddingTop: "15px",
+            }}
+          >
+            Welcome to Hyperlane Docs
+          </h1>
+          <p
+            style={{
+              color: "gray",
+              fontSize: "1rem",
+              marginBottom: "40px",
+            }}
+          >
+            Hyperlane is an open interoperability framework that enables secure
+            and seamless cross-chain communication for building interchain apps.
+          </p>
+          <div className={styles.cardContainer}>
+            <Link to={links.deployHyperlane} className={styles.card}>
+              <h3>Deploy to a New Chain</h3>
+              <p>Quickly deploy Hyperlane on a new chain</p>
+            </Link>
+            <Link to={links.deployWarpRoute} className={styles.card}>
+              <h3>Bridge Tokens with Warp Routes</h3>
+              <p>Learn how to bridge assets using Warp Routes</p>
+            </Link>
+            <Link to={links.runValidators} className={styles.card}>
+              <h3>Run a Validator</h3>
+              <p>Step-by-step instructions to run a Hyperlane Validator</p>
+            </Link>
+            <Link to={links.protocolOverview} className={styles.card}>
+              <h3>Core Concepts</h3>
+              <p>Learn about the Hyperlane protocol and core components</p>
+            </Link>
+            <Link to={links.contractAddresses} className={styles.card}>
+              <h3>Where is Hyperlane?</h3>
+              <p>Learn about the current deployments</p>
+            </Link>
           </div>
-        ))}
-      </div>
-    </section>
+        </div>
+      </section>
+    </main>
   );
 }
 
-function splitArray(array, n) {
-  let [...arr] = array;
-  var res = [];
-  while (arr.length) {
-    res.push(arr.splice(0, n));
-  }
-  return res;
-}
+export default Home;
