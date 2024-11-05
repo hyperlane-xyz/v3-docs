83048a7c594b62edc05cfac69ba966abf23c65c3
Eda Akturk <edaakturk@Edas-MacBook-Pro.local>
2024-11-05 15:33:22 +0700

    landing page mobile view

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,7 @@
 import React from "react";
-import clsx from "clsx";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageCtas from "@site/src/components/HomepageCtas";
-
-import styles from "./index.module.css";
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">Welcome to Hyperlane Docs</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}></div>
-      </div>
-    </header>
-  );
-}
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
