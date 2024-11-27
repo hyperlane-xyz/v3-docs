8fec21894289b4b4016ba18144689c4920c73f24
pbio <10051819+paulbalaji@users.noreply.github.com>
2024-11-27 18:45:32 +0000

    feat: generate default validators page from SDK

---
+++ plugins/my-loaders/index.js
@@ -0,0 +1,34 @@
+// Following the instructions on DWF blog to update Docusaurus webpack config
+// https://dwf.dev/blog/2022/11/12/2022/updating-docusaurus-webpack-config/
+module.exports = function () {
+  return {
+    name: "my-loaders",
+    configureWebpack() {
+      return {
+        module: {
+          rules: [
+            {
+              test: /\.m?js/,
+              resolve: {
+                fullySpecified: false,
+              },
+            },
+          ],
+        },
+        resolve: {
+          fallback: {
+            crypto: false,
+            stream: false,
+            assert: false,
+            http: false,
+            https: false,
+            zlib: false,
+            url: false,
+            buffer: false,
+            process: false,
+          },
+        },
+      };
+    },
+  };
+}
