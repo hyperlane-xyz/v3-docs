8b35cb1c76991f2dffe3bf007e140b21fd2f2bae
polibee <1244762327@qq.com>
2024-11-28 13:39:18 +0800

    fix: update config and links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,7 +26,7 @@ const config = {
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
   
   markdown: {
@@ -43,22 +43,15 @@ const config = {
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en", "zh-Hans"],
+    defaultLocale: "zh-Hans",
+    locales: ["zh-Hans"],
     localeConfigs: {
-      en: {
-        label: 'English',
-        htmlLang: 'en-US',
-        direction: 'ltr',
-        path: 'en'
-      },
-      'zh-Hans': {
+      "zh-Hans": {
         label: '简体中文',
-        htmlLang: 'zh-Hans',
         direction: 'ltr',
-        path: 'zh-Hans'
-      }
-    }
+        htmlLang: 'zh-Hans',
+      },
+    },
   },
 
   plugins: [
@@ -67,340 +60,37 @@ const config = {
       {
         redirects: [
           {
-            to: "/docs/guides/implementation-guide",
+            to: "/guides/implementation-guide",
             from: [
+              "/protocol/spec",
               "/docs/protocol/spec",
-              "/docs/protocol/implementation-guide",
             ],
           },
           {
-            to: "/docs/reference/messaging/messaging-interface",
-            from: ["/docs/apis-and-sdks/", "/docs/apis-and-sdks/messaging-api"],
-          },
-          {
-            to: "https://v2.hyperlane.xyz/docs/apis-and-sdks/accounts",
-            from: ["/docs/apis-and-sdks/accounts"],
-          },
-          {
-            to: "https://v2.hyperlane.xyz/docs/apis-and-sdks/query",
-            from: ["/docs/apis-and-sdks/query"],
-          },
-          {
-            to: "https://v2.hyperlane.xyz/docs/build-with-hyperlane/quickstarts/accounts",
-            from: ["/docs/build-with-hyperlane/quickstarts/accounts"],
-          },
-          {
-            to: "https://v2.hyperlane.xyz/docs/build-with-hyperlane/quickstarts/queries",
-            from: ["/docs/build-with-hyperlane/quickstarts/queries"],
-          },
-          {
-            to: "/docs/reference/applications/warp-routes",
+            to: "/operate/validators/run-validators",
             from: [
-              "/docs/apis-and-sdks/warp-api",
-              "/docs/build-with-hyperlane/examples",
-              "/docs/build-with-hyperlane/quickstarts/hyperlane-quickstart",
-              "/docs/apis-and-sdks/building-applications",
-              "/docs/apis-and-sdks/building-applications/example-usage",
-              "/docs/apis-and-sdks/building-applications/nodejs-sdk",
-              "/docs/apis-and-sdks/building-applications/example-usage/helloworld",
-              "/docs/apis-and-sdks/building-applications/example-usage/erc20-token",
-            ],
-          },
-          {
-            to: "/docs/reference/hooks/interchain-gas",
-            from: [
-              "/docs/apis-and-sdks/interchain-gas-paymaster-api",
-              "/docs/build-with-hyperlane/guides/paying-for-interchain-gas",
-              "/docs/build-with-hyperlane/guides/manually-pay-for-interchain-gas",
-              "/docs/build-with-hyperlane/guides/which-igp-to-use-and-understanding-gas-amounts",
-            ],
-          },
-          {
-            to: "/docs/reference/hooks/overview",
-            from: [
-              "/docs/apis-and-sdks/hooks",
-              "/docs/protocol/sovereign-consensus/hook-ism",
-            ],
-          },
-          {
-            to: "/docs/reference/libraries/router",
-            from: [
-              "/docs/apis-and-sdks/building-applications/writing-contracts",
-              "/docs/apis-and-sdks/building-applications/writing-contracts/router",
-              "/docs/apis-and-sdks/building-applications/writing-contracts/abacusconnectionclient",
-            ],
-          },
-          {
-            to: "/docs/reference/typescript-sdk/testing-contracts",
-            from: [
-              "/docs/apis-and-sdks/building-applications/nodejs-sdk/testing-contracts",
-            ],
-          },
-          {
-            to: "/docs/reference/typescript-sdk/deploying-contracts",
-            from: [
-              "/docs/apis-and-sdks/building-applications/nodejs-sdk/deploying-contracts",
-              "/docs/apis-and-sdks/building-applications/nodejs-sdk/contract-interaction",
-            ],
-          },
-          {
-            to: "/docs/reference/typescript-sdk/app-abstraction",
-            from: [
-              "/docs/apis-and-sdks/building-applications/nodejs-sdk/multiprovider",
-            ],
-          },
-          {
-            to: "/docs/reference/typescript-sdk/quoting-gas",
-            from: ["/docs/apis-and-sdks/building-applications/nodejs-sdk/gas"],
-          },
-          {
-            to: "/docs/intro",
-            from: [
-              "/docs/introduction",
-              "/docs/build-with-hyperlane/",
-              "/docs/introduction/readme",
-              "/docs/introduction/getting-started",
-              "/docs/introduction/why-should-you-use-hyperlane",
-            ],
-          },
-          {
-            to: "/docs/deploy-hyperlane",
-            from: [
-              "/docs/deploy",
-              "/docs/deploy/permissionless-interoperability",
-              "/docs/deploy/deploy-hyperlane",
-            ],
-          },
-          {
-            to: "/docs/your-first-message",
-            from: [
-              "/docs/build-with-hyperlane/quickstarts",
-              "/docs/build-with-hyperlane/quickstarts/messaging",
-            ],
-          },
-          {
-            to: "/docs/guides/deploy-warp-route",
-            from: [
-              "/docs/build-with-hyperlane/guides",
-              "/docs/deploy/deploy-warp-route",
-              "/docs/deploy/deploy-warp-route/deploy-a-warp-route",
-            ],
-          },
-          {
-            to: "/docs/guides/deploy-warp-route-UI",
-            from: [
-              "/docs/deploy/deploy-warp-route/deploy-the-ui-for-your-warp-route",
-            ],
-          },
-          {
-            to: "/docs/operate/validators/run-validators",
-            from: [
+              "/operators/validators",
+              "/operators/",
               "/docs/operators/validators",
               "/docs/operators/",
-              "/docs/operators/validators/setup",
             ],
           },
           {
-            to: "/docs/operate/relayer/run-relayer",
+            to: "/guides/token-faucets",
             from: [
-              "/docs/operators/relayers",
-              "/docs/operators/relayers/setup",
+              "/resources/token-sources-and-faucets",
+              "/docs/resources/token-sources-and-faucets"
             ],
           },
-          {
-            to: "/docs/protocol/ISM/third-party-ISMs/optimistic-ISM",
-            from: ["/docs/protocol/sovereign-consensus/optimistic-ism"],
-          },
-          {
-            to: "/docs/faq",
-            from: [
-              "/docs/operators/agent-keys",
-              "/docs/operators/agent-keys/hexadecimal-key-setup",
-              "/docs/operators/agent-keys/aws-setup",
-            ],
-          },
-          {
-            to: "/docs/operate/agent-config",
-            from: [
-              "/docs/operators/agent-configuration",
-              "/docs/operators/agent-configuration/configuration-reference",
-            ],
-          },
-          {
-            to: "/docs/operate/docker-quickstart",
-            from: ["/docs/operators/running-with-docker-compose"],
-          },
-          {
-            to: "/docs/operate/relayer/message-filtering",
-            from: ["/docs/operators/relayers/message-filtering"],
-          },
-          {
-            to: "/docs/operate/validators/validator-signatures-aws",
-            from: ["/docs/operators/validators/aws-setup"],
-          },
-          {
-            to: "/docs/operate/validators/monitoring-alerting",
-            from: ["/docs/operators/validators/monitoring-and-alerting"],
-          },
-          {
-            to: "/docs/protocol/protocol-overview",
-            from: [
-              "/docs/protocol/",
-              "/docs/protocol/permissionless-interoperability",
-            ],
-          },
-          {
-            to: "/docs/protocol/mailbox",
-            from: ["/docs/protocol/messaging"],
-          },
-          {
-            to: "/docs/protocol/ISM/modular-security",
-            from: ["/docs/protocol/sovereign-consensus"],
-          },
-          {
-            to: "/docs/protocol/economic-security",
-            from: ["/docs/protocol/proof-of-stake"],
-          },
-          {
-            to: "/docs/protocol/agents/watchtowers",
-            from: ["/docs/protocol/agents/processor"],
-          },
-          {
-            to: "/docs/protocol/ISM/ccip-read-ISM",
-            from: ["/docs/protocol/sovereign-consensus/ccip-read-ism"],
-          },
-          {
-            to: "/docs/protocol/ISM/multisig-ISM",
-            from: ["/docs/protocol/sovereign-consensus/multisig-ism"],
-          },
-          {
-            to: "/docs/protocol/ISM/aggregation-ISM",
-            from: ["/docs/protocol/sovereign-consensus/aggregation-ism"],
-          },
-          {
-            to: "/docs/protocol/ISM/routing-ISM",
-            from: ["/docs/protocol/sovereign-consensus/routing-ism"],
-          },
-          {
-            to: "/docs/protocol/ISM/third-party-ISMs/wormhole-ISM",
-            from: ["/docs/protocol/sovereign-consensus/wormhole-ism"],
-          },
-          {
-            to: "/docs/faq",
-            from: ["/docs/resources", "/docs/resources/faq"],
-          },
-          {
-            to: "/docs/reference/glossary",
-            from: ["/docs/resources/glossary"],
-          },
-          {
-            to: "/docs/reference/contract-addresses",
-            from: [
-              "/docs/resources/addresses",
-              "/docs/apis-and-sdks/hooks/addresses",
-              "/docs/resources/addresses/permissionless",
-            ],
-          },
-          {
-            to: "/docs/reference/domains",
-            from: [
-              "/docs/resources/domains",
-              "/docs/resources/domains/permissionless-domain-identifiers",
-            ],
-          },
-          {
-            to: "/docs/guides/explorer",
-            from: [
-              "/docs/build-with-hyperlane/explorer",
-              "/docs/build-with-hyperlane/guides/finding-my-messages",
-            ],
-          },
-          {
-            to: "/docs/guides/explorer/explorer-debugging",
-            from: [
-              "/docs/build-with-hyperlane/troubleshooting",
-              "/docs/build-with-hyperlane/explorer/observability",
-            ],
-          },
-          {
-            to: "/docs/guides/explorer/configuring-pi-chains",
-            from: ["/docs/build-with-hyperlane/explorer/configuring-pi-chains"],
-          },
-          {
-            to: "/docs/guides/explorer/rest-api",
-            from: ["/docs/build-with-hyperlane/explorer/rest-api"],
-          },
-          {
-            to: "/docs/guides/explorer/graphql-api",
-            from: ["/docs/build-with-hyperlane/explorer/graphql-api"],
-          },
-          {
-            to: "/docs/guides/token-faucets",
-            from: ["/docs/resources/token-sources-and-faucets"],
-          },
-          {
-            to: "/docs/reference/latencies",
-            from: ["/docs/resources/latencies"],
-          },
-          {
-            to: "/docs/reference/latencies",
-            from: ["/docs/guides/latencies"],
-          },
-          {
-            to: "/docs/reference/ISM/specify-your-ISM",
-            from: [
-              "/docs/resources/security",
-              "/docs/protocol/sovereign-consensus/interchain-security-modules",
-              "/docs/build-with-hyperlane/guides/receive-1",
-            ],
-          },
-          {
-            to: "/docs/protocol/interchain-gas-payment",
-            from: ["/docs/protocol/interchain-gas-payments"],
-          },
-          {
-            to: "/docs/reference/messaging/send",
-            from: ["/docs/apis-and-sdks/messaging-api/send"],
-          },
-          {
-            to: "/docs/reference/messaging/receive",
-            from: ["/docs/apis-and-sdks/messaging-api/receive"],
-          },
-          {
-            to: "/docs/guides/developer-tips/unit-testing",
-            from: ["/docs/build-with-hyperlane/guides/unit-testing"],
-          },
-          {
-            to: "/docs/protocol/economic-security/hyperlane-avs",
-            from: ["/docs/protocol/eigenlayer-avs"],
-          },
-          {
-            from: "/docs/reference/cli.mdx",
-            to: "/docs/reference/cli",
-          },
-          {
-            from: "/docs/guides/deploy-warp-route.mdx",
-            to: "/docs/guides/deploy-warp-route",
-          },
-          {
-            from: "/docs/operate/relayer/run-relayer.mdx",
-            to: "/docs/operate/relayer/run-relayer",
-          },
-          {
-            from: "/docs/protocol/economic-security/hyperlane-avs.mdx",
-            to: "/docs/protocol/economic-security/hyperlane-avs",
-          }
         ],
         createRedirects(existingPath) {
           if (existingPath.includes('/docs')) {
-            // Redirect from version-less to latest version
             return [
-              existingPath.replace('/docs', '/docs/current'),
-              // Support .mdx extension
-              `${existingPath}.mdx`,
+              existingPath.replace('/docs', ''),
+              existingPath.replace('/docs', '') + '.mdx',
             ];
           }
-          return undefined;
+          return [existingPath + '.mdx'];
         },
       },
     ],
@@ -577,7 +267,7 @@ const config = {
         },
       },
       search: {
-        language: ["en", "zh"],
+        language: ["zh"],
         hashed: true,
         indexDocs: true,
         indexBlog: true,
